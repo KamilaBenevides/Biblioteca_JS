d1502731e4aa63d9da5ca5643298dacb61722f76
Kamila Benevides <kamilaalmeida@gmail.com>
2019-08-25 17:11:46 -0300

    quase

--- js/submitUser.js
+++ js/submitUser.js
@@ -1,4 +1,4 @@
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDsI7NwHgOjhtt5j2bWPFu29Fb5CYsH06w",
     authDomain: "payroll-ba15c.firebaseapp.com",
     databaseURL: "https://payroll-ba15c.firebaseio.com",
@@ -7,31 +7,36 @@ var firebaseConfig = {
     messagingSenderId: "169965503316",
     appId: "1:169965503316:web:3303709e490565f4"
   };
-  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-    const fire = firebase.firestore();
-    
+    //const submitRecommendation = function () {
+        const firestore = firebase.firestore();
+        //const docRef = firestore.doc("users");
         const inputNome = document.querySelector("#nome");
         const inputInstutuicao = document.querySelector("#instutuicao");
         const inputMatricula = document.querySelector("#matricula");
         const inputCpf = document.querySelector("#cpf");
         const saveButton = document.querySelector("#saveB");
-
-        const docRef = firestore.doc("Usuer");
-
-        saveButton.addEventListener("click", function(){
-            docRef.set({
+        
+        console.log("chegou aqui");
+        
+        //saveButton.addEventListener("click", function(){
+            firestore.collection("usuarios").add({
             nome: inputNome.value,
             instituicao: inputInstutuicao.value,
             matricula: inputMatricula.value,
-            cpf: inputCpf.value,
-            multa: ""
-        })
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });
-    });
+            cpf: inputCpf.value
+            })
+            .then(function() {
+                console.log("Document written with ID: ");
+            })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
+        //});
+    //}
+    window.load = function () {
+        // Find the HTML element with the id recommendationForm,
+        // and when the submit event is triggered on that element, call submitRecommendation.
+        $("#recommendationForm").submit(submitRecommendation);
+    };
